e1ac829463517d8f851ed61985a78d9b5f57fba2
Andre Segura <andrensegura@gmail.com>
2016-05-20 11:32:30 -0400

    added comments and added missing include line

--- functions.php
+++ functions.php
@@ -6,19 +6,24 @@ function print_nav(){
     echo "</div>"; //navbar
 }
 
+//////////////////////
 //CHECK_LOGIN
 //TAKES username and password strings and checks it against the user table.
 //RETURNS True upon successful login, otherwise False
+//////////////////////
 function check_login($username, $password){
-
-    if ( empty($username) && empty($password) ) { //empty can be 0, empty, or not set at all.
+    //empty can be 0, empty, or not set at all.
+    if ( empty($username) && empty($password) ) {
         return False;
     } else{
         include_once('mysql.php');
         $result = query_database("SELECT password FROM users WHERE username = '". $username . "';");
         $pass_hash = $result->fetch_assoc()['password'];
 
+        //password_verify is a native PHP function:
+        //Verifies that the given hash matches the given password. Neat.
         if ( password_verify( $password, $pass_hash) ) {
+            include_once('cookie.php');
             set_cookie($username);
             return True;
         } else {
